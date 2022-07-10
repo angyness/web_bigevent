c2b4174fb03caca8cec8342cda0c941459e86c55
angyness <1439564044@qq.com>
2022-07-10 23:37:54 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,17 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+        // 需要访问权限
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+        options.complete = function(res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份证失败！') {
+                localStorage.removeItem('token')
+                location.href = './login.html'
+            }
+        }
+    }
+
+}
 })
